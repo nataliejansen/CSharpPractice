b73f0e8fae135b1fe78cf6600e6037cabea39923
nataliekuechler <nataliekuechler@outlook.com>
2023-11-24 14:27:53 -0600

    Added a practice class and moved practice problems

---
+++ CSharpPractice/ArrayPractice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    internal class ArrayPractice
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ARRAY PRACTICE:\n");
+            //ARRAY PRACTICE:
+
+            //DECLARE AND THEN REVERSE THE FOLLOWING NUMBERS IN AN ARRAY: 1, 4, 3, 2
+
+            int[] arr = { 1, 4, 3, 2 };
+            foreach (int j in arr)
+            {
+                Console.WriteLine(j);
+            }
+
+            Array.Reverse(arr);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            //RETURN THE SUM OF ALL POSITIVE NUMBERS: 1, -4, 7, 12 (THIS SHOULD OUTPUT 20)
+            Console.WriteLine("SUM OF ALL POSITIVE NUMBERS:");
+
+            int[] array = { 1, -4, 7, 12 };
+            int sum = 0;
+            for (int k = 0; k < array.Length; k++)
+            {
+                if (array[k] > 0)
+                {
+                    sum += array[k];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
--- CSharpPractice/Arrays.cs
+++ CSharpPractice/Arrays.cs
@@ -88,48 +88,6 @@ namespace CSharpPractice
                 }
             }
 
-            Console.WriteLine("ARRAY PRACTICE:\n");
-            //ARRAY PRACTICE:
-
-            //DECLARE AND THEN REVERSE THE FOLLOWING NUMBERS IN AN ARRAY: 1, 4, 3, 2
-
-            int[] arr = { 1, 4, 3, 2 };
-            foreach (int j in arr)
-            {
-                Console.WriteLine(j);
-            }
-
-            Array.Reverse(arr);
-            foreach (int i in arr)
-            {
-               Console.WriteLine(i);
-            }
-
-
-
-            //RETURN THE SUM OF ALL POSITIVE NUMBERS: 1, -4, 7, 12 (THIS SHOULD OUTPUT 20)
-            Console.WriteLine("SUM OF ALL POSITIVE NUMBERS:");
-
-            int[] array = { 1, -4, 7, 12 };
-            int sum = 0;
-            for (int k = 0; k < array.Length; k++)
-            {
-                if (array[k] > 0)
-                {
-                    sum += array[k];
-                }
-            }
-            Console.WriteLine(sum);
-
-
-
-
-
-            
-
-
-
-
         }
     }
 }
